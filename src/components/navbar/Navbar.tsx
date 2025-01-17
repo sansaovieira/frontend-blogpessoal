d81d4e171c6e5153982364f6b389c6aeda53b28d
sansaovieira <vieirasansao42@gmail.com>
2025-01-17 01:09:21 -0300

    feat(Routes): Create Routes in the App, add Login and Cadastro component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export const Navbar = () => {
     return (
         <>
@@ -5,7 +7,8 @@ export const Navbar = () => {
                        bg-indigo-900 text-white'>
 
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                    <Link to='/home' className="text-2xl font-bold">
+                    Blog Pessoal </Link>
 
                     <div className='flex gap-4'>
                         Postagens
