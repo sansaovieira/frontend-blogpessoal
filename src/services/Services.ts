85dc82c6713ae8f9095d258f450fcd939fe3f923
sansaovieira <vieirasansao42@gmail.com>
2025-01-27 09:41:36 -0300

    feat(Deploy): Project Deployment Integrative Project to resolve a fix in main

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'https://blogpessoal-7pg6.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
